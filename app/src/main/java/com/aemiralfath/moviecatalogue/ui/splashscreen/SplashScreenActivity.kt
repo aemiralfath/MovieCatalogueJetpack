7238acc5e99bdf7780e2acfb2cd4c7057f02f28a
criptdestroyer <ahmademir1201@gmail.com>
2021-04-20 17:51:45 +0700

    splash screen done

---
+++ app/src/main/java/com/aemiralfath/moviecatalogue/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.aemiralfath.moviecatalogue.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.aemiralfath.moviecatalogue.R
+import com.aemiralfath.moviecatalogue.ui.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
